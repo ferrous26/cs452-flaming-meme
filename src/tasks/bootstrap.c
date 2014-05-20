227518e4378f09647d4562b87626bb01ddf4de21
Mark Rada <marada@uwaterloo.ca>
2014-05-20 17:44:58 -0400

    Cleanup the bootstrap and hello tasks

--- src/tasks/bootstrap.c
+++ src/tasks/bootstrap.c
@@ -6,15 +6,15 @@
 #include <bootstrap.h>
 
 static void print_created(int tid) {
-    debug_log("Created: %d\n", tid);
+    debug_log("Created: %d", tid);
     vt_flush();
 }
 
 void bootstrap(void) {
-    print_created(Create(8, hello));
-    print_created(Create(8, hello));
     print_created(Create(1, hello));
     print_created(Create(1, hello));
+    print_created(Create(8, hello));
+    print_created(Create(8, hello));
 
     debug_log("First: exiting");
     vt_flush();
--- src/tasks/hello.c
+++ src/tasks/hello.c
@@ -1,12 +1,12 @@
 
 #include <io.h>
 #include <syscall.h>
-
+#include <debug.h>
 #include <hello.h>
 #include <debug.h>
 
 static void print_info(int tid, int pid) {
-    debug_log("Id: %d Parent: %d\n", tid, pid);
+    debug_log("Id: %d Parent: %d", tid, pid);
     vt_flush();
 }
 
