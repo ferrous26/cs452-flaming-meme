88f0dce956950ab54f370275b3afef6a5902f6f2
Mark Rada <marada@uwaterloo.ca>
2014-05-20 19:02:52 -0400

    Add ctx benchmarking to bootstrap.c

--- src/tasks/bootstrap.c
+++ src/tasks/bootstrap.c
@@ -1,7 +1,7 @@
 #include <io.h>
 #include <debug.h>
 #include <syscall.h>
-
+#include <benchmark.h>
 #include <hello.h>
 #include <bootstrap.h>
 
@@ -10,11 +10,22 @@ static void print_created(int tid) {
     vt_flush();
 }
 
+BENCH(ctx);
+
 void bootstrap(void) {
     print_created(Create(1, hello));
     print_created(Create(1, hello));
     print_created(Create(8, hello));
     print_created(Create(8, hello));
+
+    BENCH_START(ctx);
+    for (size i = 0; i < 10000; i++) {
+	Pass();
+	BENCH_LAP(ctx, 0);
+    }
+
+    BENCH_PRINT_WORST(ctx);
+    BENCH_PRINT_AVERAGE(ctx);
 
     debug_log("First: exiting");
     vt_flush();
