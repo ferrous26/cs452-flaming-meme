994cfe7205b2726a3239fe83bc27a3e01c28654f
Mark Rada <marada@uwaterloo.ca>
2014-05-22 14:39:36 -0400

    Allow circular buffer stuff to be more inalienable

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -3,6 +3,7 @@
 #define __CIRCULAR_BUFFER_H__
 
 #include <std.h>
+#include <math.h>
 
 /**
  * A circular buffer is simply an array buffer which wraps around
@@ -17,8 +18,9 @@
  *       the buffer should be located directly after the buffer in
  *       memory. These properties make it easier for the compiler and
  *       hardware to optimize access.
- * @note It is beneficial to choose a buffer length that is a power of
- *       2 as the compiler can better optimize access.
+ * @note It is _required_ to choose a buffer length that is a power of
+ *       2 as the compiler can better optimize access, and also because
+ *       the implementation will break otherwise.
  */
 struct circular_buffer {
     size head;     // index of next producer slot
@@ -26,32 +28,60 @@ struct circular_buffer {
     size length;   // total length of the buffer
 };
 
-/** A circular buffer specialized for type char */
+
+/**
+ * Generic implementation for all circular buffers
+ */
+#define CB_INIT(type)				\
+  cb->cb.head   = 0;				\
+  cb->cb.tail   = 0;				\
+  cb->cb.length = length;			\
+  cb->buffer    = (type*)buffer;
+
+#define CB_PRODUCE				\
+  size ptr = cb->cb.head;			\
+  cb->buffer[ptr] = value;			\
+  cb->cb.head = mod2(ptr + 1, cb->cb.length);
+
+#define CB_CAN_CONSUME				\
+  return (bool)(cb->cb.head != cb->cb.tail);
+
+#define CB_CONSUME(type)			\
+  size ptr    = cb->cb.tail;			\
+  type value  = cb->buffer[ptr];		\
+  cb->cb.tail = mod2(ptr + 1, cb->cb.length);	\
+  return value;
+
+
+/**
+ * A circular buffer specialized for type char
+ */
 typedef struct circular_buffer_char {
     struct circular_buffer cb;
     char* buffer;
 } char_buffer;
 
-/** A circular buffer specialized for type uint32 */
+static void cbuf_init(char_buffer* const cb,
+		      const size         length,
+		      const char* const  buffer)                  { CB_INIT(char); }
+static void cbuf_produce(char_buffer* const cb, const char value) { CB_PRODUCE; }
+static bool cbuf_can_consume(const char_buffer* const cb)         { CB_CAN_CONSUME; }
+static char cbuf_consume(char_buffer* const cb)                   { CB_CONSUME(char); }
+
+
+/**
+ * A circular buffer specialized for type uint32
+ */
 typedef struct circular_buffer_uint32 {
     struct circular_buffer cb;
     uint32* buffer;
 } uint_buffer;
 
-void cbuf_init(char_buffer* const  cb,
-	       const size          length,
-	       const char* const   buffer);
-void ibuf_init(uint_buffer* const  cb,
-	       const size          length,
-	       const uint32* const buffer);
-
-void cbuf_produce(char_buffer*  const cb, const char   value);
-void ibuf_produce(uint_buffer*  const cb, const uint32 value);
-
-bool cbuf_can_consume(const char_buffer*  const cb);
-bool ibuf_can_consume(const uint_buffer*  const cb);
-
-char   cbuf_consume(char_buffer*  const cb);
-uint32 ibuf_consume(uint_buffer*  const cb);
+static void ibuf_init(uint_buffer* const  cb,
+		      const size          length,
+		      const uint32* const buffer)                   { CB_INIT(uint32); }
+static void ibuf_produce(uint_buffer* const cb, const uint32 value) { CB_PRODUCE; }
+static bool ibuf_can_consume(const uint_buffer* const cb)           { CB_CAN_CONSUME; }
+static uint32 ibuf_consume(uint_buffer* const cb)                   { CB_CONSUME(uint32); }
 
 #endif
