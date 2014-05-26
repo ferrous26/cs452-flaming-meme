eca698a24a93ac40c773d599b49e96452cea3760
Nik Illerbrun <nik.illerbrun@gmail.com>
2014-05-26 19:18:51 -0400

    added code to hopefully run the name server

--- include/tasks/name_server.h
+++ include/tasks/name_server.h
@@ -2,6 +2,24 @@
 #ifndef __NAME_SERVER_H__
 #define __NAME_SERVER_H__
 
+#define NAME_MAX_SIZE 8
+#define NAME_OVERLAY_SIZE (NAME_MAX_SIZE/4)
+#define NAME_MAX      32
+
+typedef enum {
+    REGISTER,
+    LOOKUP
+} ns_req_type;
+
+typedef struct {
+    ns_req_type type;
+    union {
+        char text[NAME_MAX_SIZE];
+	uint32 overlay[NAME_OVERLAY_SIZE];
+    } payload;
+} ns_req;
+
+
 void name_server(void);
 
 #endif
