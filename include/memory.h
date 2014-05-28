c122448e6ac899c8febe65549b0bd7b9491614ad
Mark Rada <marada@uwaterloo.ca>
2014-05-28 16:07:34 -0400

    First round of optimizations for memcpy

--- include/memory.h
+++ include/memory.h
@@ -1,10 +1,5 @@
 #include <std.h>
 
-void memcpy(void* destaddr, const void* srcaddr, size len);
-void memcpy4(void* destaddr, const void* srcaddr, size len);
-void memcpy8(void* destaddr, const void* srcaddr, size len);
-void memcpy16(void* destaddr, const void* srcaddr, size len);
-void memcpy32(void* destaddr, const void* srcaddr, size len);
+void* memcpy(void* destaddr, const void* srcaddr, size len);
 
 int memcmp(const void* left, const void* right, const size len);
-int memcmp4(const void* left, const void* right, const size len);
--- src/memory.asm
+++ src/memory.asm
@@ -1,46 +1,61 @@
 	.file	"memory.asm"
 	.text
 	.align	2
-	.global memcpy4, memcpy8, memcpy16, memcpy32, memcmp, memcmp4
+	.global memcpy, memcmp
 	.type	memcpy,   %function
-	.type	memcpy4,  %function
-	.type	memcpy8,  %function
-	.type	memcpy16, %function
-	.type	memcpy32, %function
 	.type	memcmp,   %function
-	.type	memcmp4,  %function
 
-memcpy32:
-	subs r2, r2, #32
-	ldmplia r1!, {r3-r10}
-	stmplia r0!, {r3-r10}
-	bne memcpy32
-	mov pc, lr
-	.size   memcmp32, .-memcmp32
+memcpy:
+	/** r0 = dest, r1 = src, r2 = len **/
 
-memcpy16:
-	subs r2, r2, #16
-	ldmplia r1!, {r3-r6}
-	stmplia r0!, {r3-r6}
-	bne memcpy16
-	mov pc, lr
-	.size   memcmp16, .-memcmp16
+	/* Back up r0, since we must return it */
+	stmfd sp!, {r0, r4-r10}
 
-memcpy8:
-	subs r2, r2, #8
-	ldmplia r1!, {r3-r4}
-	stmplia r0!, {r3-r4}
-	bne memcpy8
-	mov pc, lr
-	.size   memcmp8, .-memcmp8
+	/* If already word aligned, skip to accelerated case */
+	ands   r3, r0, #3
+	andeqs r3, r1, #3
+	beq    .fastcpy
 
-memcpy4:
-	subs r2, r2, #4
-	ldmplia r1!, {r3}
-	stmplia r0!, {r3}
-	bne memcpy4
-	mov pc, lr
-	.size   memcmp4, .-memcmp4
+	/* If we got here, we want to try and align the addresses */
+.align_it:
+	ldrb r3, [r1]
+	add  r1, r1, #1
+	subs r2, r2, #1   /* set condition flags here */
+	strb r3, [r0]
+	add  r0, r0, #1
+	bne .align_it
+
+	/* check alignment again */
+	ands    r3, r0, #3
+	andeqs  r3, r1, #3
+	bne     .slowcpy
+
+/* fall through to the fastcpy case! */
+
+.fastcpy:
+  /* more than 32? */
+
+32:
+  subs  r2, r2, #32
+  ldmplia r1!, {r3-r10}
+  stmplia r1!, {r3-r10}
+  bpl 32 /* if there is something left, go back to start */
+
+  /* more than 16? */
+	/*mov  pc, lr*/
+
+  add r2, r2, #32
+
+.slowcpy:
+	ldrb r3, [r1]
+	add r1, r1, #1
+	subs r2, r2, #1
+	strb r3, [r0]
+	add r0, r0, #1
+	bne .slowcpy
+
+	ldmfd sp!, {r0, r4-r10}
+	mov  pc, lr
 
 memcmp:
 	ldrb r3, [r0]
@@ -59,19 +74,3 @@ memcmp:
 	mov r0, #0
 	mov pc, lr
 	.size   memcmp, .-memcmp
-
-memcmp4:
-	ldmia r0!, {r3}
-	ldmia r1!, {r4}
-
-	cmp r3, r4
-	movmi r0, #-1
-	movmi pc, lr
-	movne r0, #1
-	movne pc, lr
-
-	subs r2, r2, #4
-	bne memcmp4
-	mov r0, #0
-	mov pc, lr
-	.size   memcmp4, .-memcmp4
