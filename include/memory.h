d87e190c3739c5c093903e68bdc184dfce28cd49
Mark Rada <marada@uwaterloo.ca>
2014-05-28 22:29:12 -0400

    Cleanup memcpy for this assignment

--- include/memory.h
+++ include/memory.h
@@ -2,4 +2,6 @@
 
 void* memcpy(void* destaddr, const void* srcaddr, size len);
 
-int memcmp(const void* left, const void* right, const size len);
+//void* memset(void *buffer, int c, size len);
+
+//int memcmp(const void* left, const void* right, const size len);
--- src/memory.asm
+++ src/memory.asm
@@ -1,13 +1,11 @@
 	.file	"memory.asm"
 	.text
 	.align	2
-	.global memcpy, memcmp
-	.type	memcpy,   %function
-	.type	memcmp,   %function
 
+	.global memcpy
+	.type	memcpy,   %function
 memcpy:
 	/** r0 = dest, r1 = src, r2 = len **/
-
 	/* Back up r0, since we must return it */
 	stmfd sp!, {r0, r4-r10}
 
@@ -17,7 +15,7 @@ memcpy:
 	beq    .fastcpy
 
 	/* If we got here, we want to try and align the addresses */
-  sub  r2, r2, r3
+	sub  r2, r2, r3
 .align:
 	ldrb r4, [r1]
 	add  r1, r1, #1
@@ -29,40 +27,24 @@ memcpy:
 	/* check alignment again */
 	ands    r3, r0, #3
 	andeqs  r3, r1, #3
-	bne     .slowcpy
+	bne     .slowcpy /* if still not aligned, then give up */
 
-/* fall through to the fastcpy case! */
-
-.fastcpy:
-
-/* more than 32? */
-.big:
-  subs r2, r2, #32
-  ldmplia r1!, {r3-r10}
-  stmplia r0!, {r3-r10}
-  beq .done
-  bpl .big /* if there is something left, go back to start */
-
-  add r2, r2, #32
-
-.medium:
-  subs r2, r2, #8
-  ldmplia r1!, {r3-r4}
-  stmplia r0!, {r3-r4}
-  beq .done
-  bpl .medium /* if there is something left, go back to start */
-
-  add r2, r2, #8
+.fastcpy: /* fall through to the fastcpy case! */
+.big: /* more than 32? */
+	subs r2, r2, #32
+	ldmplia r1!, {r3-r10}
+	stmplia r0!, {r3-r10}
+	beq .done
+	bpl .big /* if there is something left, go back to start */
+	add r2, r2, #32
 
 .small:
-  subs r2, r2, #4
-  ldmplia r1!, {r3}
-  stmplia r0!, {r3}
-  beq .done
-  bpl .small /* if there is something left, go back to start */
-
-  add r2, r2, #4
-
+	subs r2, r2, #4
+	ldmplia r1!, {r3}
+	stmplia r0!, {r3}
+	beq .done
+	bpl .small /* if there is something left, go back to start */
+	add r2, r2, #4
 
 .slowcpy:
 	ldrb r3, [r1]
@@ -75,7 +57,20 @@ memcpy:
 .done:
 	ldmfd sp!, {r0, r4-r10}
 	mov  pc, lr
+	.size	memcpy, .-memcpy
 
+	/*
+	.global memset
+	.type	memset,   %function
+memset:
+	stmfd sp!, {r0, r4-r10}
+	// do shit
+	ldmfd sp!, {r0, r4-r10}
+	mov pc, lr
+	.size   memset, .-memset
+
+	.global memcmp
+	.type	memcmp,   %function
 memcmp:
 	ldrb r3, [r0]
 	ldrb r4, [r1]
@@ -93,3 +88,4 @@ memcmp:
 	mov r0, #0
 	mov pc, lr
 	.size   memcmp, .-memcmp
+	*/
