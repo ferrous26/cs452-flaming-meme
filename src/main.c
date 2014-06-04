f10ff006f41ee812b52fd0f4f4b66bfbb5abc95a
Mark Rada <marada@uwaterloo.ca>
2014-06-04 01:12:34 -0400

    Flush caches during shutdown()

--- src/main.c
+++ src/main.c
@@ -59,6 +59,7 @@ static inline void _init() {
 void shutdown(void) {
     vt_log("Shutting Down");
     vt_deinit();
+    _flush_caches();
     asm volatile ("mov	pc, %0" : : "r" (exit_point));
 }
 
