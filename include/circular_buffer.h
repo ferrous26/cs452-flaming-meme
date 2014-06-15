7bca0aa09513401f6c225cf5922911e322b41f9d
Mark Rada <marada@uwaterloo.ca>
2014-06-14 20:08:11 -0400

    Add #count to circular buffer
    Not a big deal anymore since circular buffer is not used in any
    performance critical places.

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -25,6 +25,7 @@ struct circular_buffer {
     size head;     // index of next producer slot
     size tail;     // index of next consumer slot
     size length;   // total length of the buffer
+    size count;    // current size of the buffer
 };
 
 
@@ -35,12 +36,14 @@ struct circular_buffer {
   cb->cb.head   = 0;				\
   cb->cb.tail   = 0;				\
   cb->cb.length = length;			\
+  cb->cb.count  = 0;				\
   cb->buffer    = (type*)buffer;
 
-#define CB_PRODUCE				\
-  size ptr = cb->cb.head;			\
-  cb->buffer[ptr] = value;			\
-  cb->cb.head = mod2(ptr + 1, cb->cb.length);
+#define CB_PRODUCE					\
+    size ptr = cb->cb.head;				\
+    cb->buffer[ptr] = value;				\
+    cb->cb.count = mod2(ptr + 1, cb->cb.length);	\
+    cb->cb.head  = mod2(ptr + 1, cb->cb.length);
 
 #define CB_CAN_CONSUME				\
   return (bool)(cb->cb.head != cb->cb.tail);
@@ -49,8 +52,11 @@ struct circular_buffer {
   size ptr    = cb->cb.tail;			\
   type value  = cb->buffer[ptr];		\
   cb->cb.tail = mod2(ptr + 1, cb->cb.length);	\
+  cb->cb.count--;				\
   return value;
 
+#define CB_COUNT				\
+    return cb->cb.count
 
 /**
  * A circular buffer specialized for type char
@@ -66,6 +72,7 @@ static inline void cbuf_init(char_buffer* const cb,
 static inline void cbuf_produce(char_buffer* const cb, const char value) { CB_PRODUCE; }
 static inline bool cbuf_can_consume(const char_buffer* const cb)         { CB_CAN_CONSUME; }
 static inline char cbuf_consume(char_buffer* const cb)                   { CB_CONSUME(char); }
+static inline char cbuf_count(char_buffer* const cb)                     { CB_COUNT; }
 
 
 /**
@@ -82,5 +89,6 @@ static inline void ibuf_init(uint_buffer* const  cb,
 static inline void ibuf_produce(uint_buffer* const cb, const uint32 value) { CB_PRODUCE; }
 static inline bool ibuf_can_consume(const uint_buffer* const cb)           { CB_CAN_CONSUME; }
 static inline uint32 ibuf_consume(uint_buffer* const cb)                   { CB_CONSUME(uint32); }
+static inline uint32 ibuf_count(uint_buffer* const cb)                     { CB_COUNT; }
 
 #endif
