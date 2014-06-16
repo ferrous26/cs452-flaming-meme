dd5be101ee281df847a40b22cb64c1018991b087
Mark Rada <marada@uwaterloo.ca>
2014-06-16 16:42:47 -0400

    Assertion failures will now try to clear the screen

--- src/debug.c
+++ src/debug.c
@@ -21,10 +21,10 @@ void debug_assert_fail(const char* const file,
     char buffer[512];
     char* ptr = buffer;
 
-    ptr = log_start(ptr);
+    ptr = vt_clear_screen(ptr);
+    ptr = vt_goto(ptr, 10, 10);
     ptr = sprintf(ptr, "assertion failure at %s:%u\n\r", file, line);
     ptr = sprintf_va(ptr, msg, args);
-    ptr = log_end(ptr);
 
     uart2_bw_write(buffer, (uint)(ptr - buffer));
     va_end(args);
