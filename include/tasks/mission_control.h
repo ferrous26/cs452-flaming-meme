01defbe0422a5bb5446cb3a4ef0a28f99f294118
Nik Illerbrun <nik.illerbrun@gmail.com>
2014-06-19 15:27:22 -0400

    added a task to maintain track state and poll for sensor data

---
+++ include/tasks/mission_control.h
@@ -0,0 +1,11 @@
+
+#ifndef __MISSION_CONTROL_H__
+#define __MISSION_CONTROL_H__
+
+#define MISSION_CONTROL_NAME "BORIS"
+#define SENSOR_POLL_NAME     "SR_POLL"
+
+void mission_control(void) __attribute__((noreturn));
+
+#endif
+
