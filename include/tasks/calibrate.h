0b7de15ada68efc618c7f730acb310a99ef67ff8
Mark Rada <marada@uwaterloo.ca>
2014-06-28 15:24:52 -0400

    Implement a task for collecting velocity information

--- include/tasks/calibrate.h
+++ include/tasks/calibrate.h
@@ -2,6 +2,7 @@
 #ifndef __CALIBRATE_H__
 #define __CALIBRATE_H__
 
+void velocitate(void);
 void accelerate(void);
 void calibrate(void);
 
--- src/tasks/calibrate.c
+++ src/tasks/calibrate.c
@@ -7,6 +7,107 @@
 #include <tasks/train_station.h>
 #include <tasks/mission_control.h>
 
+void velocitate() {
+
+    int tid = 0;
+    int train_num = 0;
+    Receive(&tid, (char*)&train_num, sizeof(train_num));
+    Reply(tid, NULL, 0);
+
+    update_turnout(1, 'S');
+    update_turnout(2, 'S');
+    update_turnout(3, 'C');
+    update_turnout(4, 'S');
+    update_turnout(5, 'C');
+    update_turnout(6, 'S');
+    update_turnout(7, 'S');
+    update_turnout(8, 'C');
+    update_turnout(9, 'S');
+    update_turnout(10, 'S');
+    update_turnout(11, 'C');
+    update_turnout(12, 'C');
+    update_turnout(13, 'S');
+    update_turnout(14, 'S');
+    update_turnout(15, 'S');
+    update_turnout(16, 'S');
+    update_turnout(17, 'S');
+    update_turnout(18, 'C');
+    update_turnout(153, 'C');
+    update_turnout(154, 'S');
+    update_turnout(155, 'C');
+    update_turnout(156, 'S');
+
+    log("Please get train %d to sensor C9", train_num);
+
+    // wait for train to get to right spot
+    delay_sensor('C', 9);
+    // get it into the proper starting position
+    train_reverse(train_num);
+    train_set_speed(train_num, 14);
+    Delay(50);
+    train_set_speed(train_num, 10);
+    train_reverse(train_num);
+    train_set_speed(train_num, 14);
+
+    int start;
+    int curved;
+    int straight;
+
+    // GO GO GO!
+    for (int speed = 14; speed; speed--) {
+        train_set_speed(train_num, speed);
+
+        for (int count = 0; count < 2; count++) {
+            delay_sensor('C', 9);
+            start = Time();
+
+            delay_sensor('B', 15);
+            curved = Time() - start;
+
+            delay_sensor('C', 13);
+            start = Time();
+
+            delay_sensor('E', 7);
+            straight = Time() - start;
+
+            // SPEED UP UNTIL WE CLOSE IN AGAIN
+            train_set_speed(train_num, 14);
+            log("%d,%d,%d,%d", train_num, speed, curved, straight);
+
+            // getting close, set speed to correct value
+            delay_sensor('D', 13);
+            train_set_speed(train_num, speed);
+        }
+    }
+
+    train_set_speed(train_num, 1);
+
+    for (int speed = 1; speed < 15; speed++) {
+        train_set_speed(train_num, speed);
+
+        for (int count = 0; count < 2; count++) {
+            delay_sensor('C', 9);
+            start = Time();
+
+            delay_sensor('B', 15);
+            curved = Time() - start;
+
+            delay_sensor('C', 13);
+            start = Time();
+
+            delay_sensor('E', 7);
+            straight = Time() - start;
+
+            // SPEED UP UNTIL WE CLOSE IN AGAIN
+            train_set_speed(train_num, 14);
+            log("%d,%d,%d,%d", train_num, speed, curved, straight);
+
+            // getting close, set speed to correct value
+            delay_sensor('D', 13);
+            train_set_speed(train_num, speed);
+        }
+    }
+}
 
 void accelerate() {
 
