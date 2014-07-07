e590329059edafa461324397f550cf314e35be70
Mark Rada <marada@uwaterloo.ca>
2014-07-07 05:17:24 -0400

    Add a max routing time estimate (in clock ticks)

--- include/dijkstra.h
+++ include/dijkstra.h
@@ -4,10 +4,11 @@
 
 #include <track_node.h>
 
+#define MAX_ROUTING_TIME_ESTIMATE 5
+
 int dijkstra(const track_node* const track,
              const track_node* const start,
              const track_node* const end,
              path_node* const path);
 
 #endif
-
