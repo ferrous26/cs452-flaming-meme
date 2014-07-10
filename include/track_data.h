b8f85ce6db62b4c53b9dda7c5954195e4cd21d21
Mark Rada <marada@uwaterloo.ca>
2014-07-10 01:18:19 -0400

    Regenerate track data

--- include/track_data.h
+++ include/track_data.h
@@ -3,11 +3,12 @@
 #define __TRACK_DATA_H__
 
 #include <track_node.h>
+#include <std.h>
 
 // The track initialization functions expect an array of this size.
 #define TRACK_MAX 144
 
-void init_tracka(track_node* const track);
-void init_trackb(track_node* const track);
+void init_tracka(track_node* const track) TEXT_COLD;
+void init_trackb(track_node* const track) TEXT_COLD;
 
 #endif
