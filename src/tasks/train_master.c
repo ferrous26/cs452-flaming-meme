48302a14d485aec3a2cdac96812f3e9ce55fc28d
Mark Rada <marada@uwaterloo.ca>
2014-07-18 16:08:03 -0400

    Don’t forget to reply to Lenin

--- src/tasks/train_master.c
+++ src/tasks/train_master.c
@@ -142,6 +142,9 @@ static void master_init(master* const ctxt) {
     ctxt->train_id  = init[0];
     ctxt->train_gid = pos_to_train(ctxt->train_id);
 
+    result = Reply(tid, NULL, 0);
+    if (result != 0)
+        ABORT("[Aunt] Failed to wake up Lenin (%d)", result);
 
     sprintf(ctxt->name, "AUNT%d", ctxt->train_gid);
     ctxt->name[7] = '\0';
