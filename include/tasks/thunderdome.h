255b3e8345b378d142513a56547dda43aab11158
Mark Rada <marada@uwaterloo.ca>
2014-08-22 15:52:12 -0400

    Throw out all the train shit and reports before kernel4

--- include/tasks/thunderdome.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __THUNDERDOME_H__
-#define __THUNDERDOME_H__
-
-#include <std.h>
-
-void thunderdome(void);
-
-#endif
--- include/tasks/train_console.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __TRAIN_CONSOLE_H__
-#define __TRAIN_CONSOLE_H__
-
-void __attribute__((noreturn)) train_console(void);
-
-#endif
-
